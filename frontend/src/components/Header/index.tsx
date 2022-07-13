81d4d1e79540a3bfec946b96ae63465f7552fbc3
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2022-07-13 13:02:54 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por Jorge Rufino <br />                
+                    <a href="https://github.com/JorgeRufinoProgammer">@github/JorgeRufinoProgammer</a><br />                
+                    <a href="https://www.linkedin.com/in/jorge-rufino-ab156434/">@linkedin/JorgeRufino</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
